a0ffc79cb6ecf022573274f05a4c0c7b9ef399a6
Steven Burns <smburns47@gmail.com>
2020-12-28 10:57:28 -0800

    Easy using recursive generators

--- 19/test_A.py
+++ 19/test_A.py
@@ -7,55 +7,7 @@ class Leaf:
     def __repr__(self):
         return self.c
 
-from logging import debug
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-def isvalid( tbl, state, seq):
-
-    debug("="*80)
-    debug(f"isvalid: {state} {seq}")
-
-    def aux( lst_of_lst, *, level):
-        nonlocal cursor
-        debug( f"{' '*(3*level)}aux: lol: {lst_of_lst} cursor: {cursor} remaining: {seq[cursor:]}")
-        save_cursor = cursor
-        for terms in lst_of_lst:
-            debug(f"{' '*(3*level)}setting cursor to {save_cursor}")
-            cursor = save_cursor
-            failed = False
-            for term in terms:
-                if failed: continue
-                debug( f"{' '*(3*level)}term {term} cursor {cursor} remaining {seq[cursor:]}")
-                if type(term) == Leaf:
-                    debug( f"{' '*(3*level)}Leaf seq {seq} cursor {cursor} term.c {term.c}")
-                    if cursor < len(seq) and term.c == seq[cursor]:
-                        debug( f"{' '*(3*level)}Match")
-                        cursor += 1
-                    else:
-                        debug( f"{' '*(3*level)}No Match")
-                        failed = True
-                else:
-                    debug( f"{' '*(3*level)}non-Leaf: term {term} cursor {cursor} remaining {seq[cursor:]}")
-                    if not aux( tbl[term], level=level+1):
-                        failed = True
-                debug( f"{' '*(3*level)}end of terms loop: cursor {cursor} remaining {seq[cursor:]}")
-
-            debug( f"{' '*(3*level)}end of lol loop: failed {failed} level {level} cursor {cursor}")
-            if not failed and level > 0:
-                return True
-            if not failed and level == 0 and cursor == len(seq):
-                debug( f"{' '*(3*level)}Found")
-                return True
-
-        return False
-
-    cursor = 0
-    return aux( tbl[state], level=0)
-
-def main(fp):
-
+def parse(fp):
     messages_now = False
     messages = []
 
@@ -86,17 +38,46 @@ def main(fp):
         if not messages_now:
             expr.append(term)
             tbl[state] = expr
-    debug(tbl,messages)
+    return tbl, messages
+
+def isvalid( tbl, message):
+    
+    def gen0( rule, cursor):
+        if type(rule) == Leaf:
+            if cursor < len(message) and message[cursor] == rule.c:
+                yield cursor+1
+        else:
+            yield from gen2( tbl[rule], cursor)
+
+    def gen1( lst, cursor):
+        if len(lst) == 1:
+            yield from gen0( lst[0], cursor)
+        else:
+            for next_cursor in gen0( lst[0], cursor):
+                yield from gen1( lst[1:], next_cursor)
+
+    def gen2( lol, cursor):
+        for lst in lol:
+            yield from gen1(lst, cursor)
+
+    for next_cursor in gen0( 0, 0):
+        if next_cursor == len(message):
+            return True
+
+    return False
+
+
+def main(fp):
+    tbl, messages = parse(fp)
 
     count = 0
     for message in messages:
-        if isvalid( tbl, 0, message):
+        if isvalid( tbl, message):
             count += 1
 
     print(f"count: {count}")
     return count
     
-@pytest.mark.skip
 def test_A():
     txt = """0: 4 1 5
 1: 2 3 | 3 2
@@ -115,7 +96,6 @@ aaaabbb
     with io.StringIO(txt) as fp:
         assert main(fp) == 2
 
-@pytest.mark.skip
 def test_AA():
     txt = """0: 4 5 | 4 0 5
 4: "a"
@@ -131,7 +111,6 @@ aabbb
     with io.StringIO(txt) as fp:
         assert main(fp) == 3
 
-@pytest.mark.skip
 def test_AAA():
     txt = """0: 2
 2: "a" 2 "a" | "a" "a"
@@ -144,7 +123,6 @@ a
     with io.StringIO(txt) as fp:
         assert main(fp) == 2
 
-@pytest.mark.skip
 def test_AAAA():
     txt = """0: 1
 1: "b" 1 | "b"
@@ -163,7 +141,6 @@ bbb
     with io.StringIO(txt) as fp:
         assert main(fp) == 1
 
-@pytest.mark.skip
 def test_B():
     txt = """0: 1 2
 1: "a"
@@ -178,12 +155,10 @@ b
     with io.StringIO(txt) as fp:
         assert main(fp) == 1
 
-@pytest.mark.skip
 def test_C():
     with open( "data", "rt") as fp:
         main(fp)
 
-@pytest.mark.skip
 def test_D():
     with open( "data1", "rt") as fp:
         main(fp)
