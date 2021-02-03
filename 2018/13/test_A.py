df5123a7b40b732f4fff19e2b2dea39bfdf7bafc
Steven Burns <smburns47@gmail.com>
2021-02-03 04:43:43 -0800

    A lot of details

--- 2018/13/test_A.py
+++ 2018/13/test_A.py
@@ -4,214 +4,218 @@ import re
 import itertools
 from collections import defaultdict
 
+dir2c = { (1,0): 'v', (-1,0): '^', (0,1): '>', (0,-1): '<'}
+c2dir = { v: k for k,v in dir2c.items()}
+next_state = { 'L': 'S', 'S': 'R', 'R': 'L'}
+left_turn = { a:b for a,b in zip("<>^v", "v^<>")}
+right_turn = { a:b for a,b in zip("<>^v", "^v><")}
+forward_slash = { a:b for a,b in zip("<>^v", "v^><")}
+backward_slash = { a:b for a,b in zip("<>^v", "^v<>")}
+
+class Cart:
+    def __init__( self, irow, icol, c):
+        self.irow = irow
+        self.icol = icol
+        self.drow = None
+        self.dcol = None
+        self.set_dir(c)
+        self.state = 'L'
+
+    def toPos(self):
+        return self.irow,self.icol
+
+    def __repr__(self):
+        return str( (self.irow,self.icol,self.drow,self.dcol,self.state))
+
+    def set_dir(self, c):
+        self.drow, self.dcol = c2dir[c]
+
+    def incr_state(self):
+        self.state = next_state[self.state]
+
 def parse(fp):
-    p_init = re.compile(r'^initial state: ((#|\.)+)\s*$')
-    p_blank = re.compile('^\s*$')
-    p_rule = re.compile(r'^((#|\.)+)\s+=>\s+((\#|\.)+)\s*$')
-
-    initial_state = None
-    rules = []
-
+    seq = []
     for line in fp:
         line = line.rstrip('\n')
-        m = p_init.match(line)
-        if m:
-            initial_state = m.groups()[0]
-            continue
-        m = p_blank.match(line)
-        if m:
-            continue
-        m = p_rule.match(line)
-        if m:
-            rules.append( (m.groups()[0], m.groups()[2]))
-            continue
-        assert False, line
+        seq.append(line)
+    return seq
 
-    return initial_state, rules
+def extract_carts(board):
+    carts = []
+    newboard = []
+    for i,line in enumerate(board):
+        newline = ''
+        for j,c in enumerate(line):
+            if c in "<>^v":
+                carts.append( Cart(i,j,c))
+                if c in "<>":
+                    newline += "-"
+                else:
+                    newline += "|"
+            else:
+                newline += c
+        newboard.append(newline)
+            
+    return carts, newboard
+
+def print_board( carts, board):
+    positions = defaultdict(list)
+    for idx,cart in enumerate(carts):
+        positions[cart.toPos()].append(idx)
+
+    for i,line in enumerate(board):
+        newline = ''
+        for j,c in enumerate(line):
+            if (i,j) in positions:
+                if len(positions[(i,j)]) > 1:
+                    newc = 'X'
+                else:
+                    cart = carts[positions[(i,j)][0]]
+                    newc = dir2c[(cart.drow,cart.dcol)]
+            else:
+                newc = c
+            newline += newc
+        print(newline)
+
+def step( carts, board):
+    first_crash_location = None
+    positions = { cart.toPos() for cart in carts}
+    for cart in carts:
+        # Should always be able to go forward one
+        jrow,jcol = cart.irow+cart.drow,cart.icol+cart.dcol
+        c = board[jrow][jcol] 
+        assert c != ' '
+        d = cart.drow,cart.dcol
+        if c == '+':
+            """change dir based on state"""
+            if cart.state == 'L':
+                cart.drow,cart.dcol = c2dir[left_turn[dir2c[d]]]
+            elif cart.state == 'R':
+                cart.drow,cart.dcol = c2dir[right_turn[dir2c[d]]]
+            elif cart.state == 'S':
+                pass
+            else:
+                assert False, (cart.drow,cart.dcol,cart.state)
+            cart.incr_state()
+        elif c == '/':
+            cart.drow,cart.dcol = c2dir[forward_slash[dir2c[d]]]
+        elif c == '\\':
+            cart.drow,cart.dcol = c2dir[backward_slash[dir2c[d]]]
+        else:
+            pass
+
+        positions.remove(cart.toPos())
+
+        cart.irow,cart.icol = jrow,jcol
+        if first_crash_location is None and cart.toPos() in positions:
+            first_crash_location = cart.toPos()
+            
+        positions.add(cart.toPos())
+
+    carts.sort(key=lambda o: o.toPos())
+    return first_crash_location
+
 
 
 def main(fp):
-    initial_state, rules = parse(fp)
+    board = parse(fp)
 
-    line = defaultdict(lambda: '.')
+    print('Raw board')
+    for line in board:
+        print(line)
 
-    for idx,c in enumerate(initial_state):
-        if c == '#':
-            line[idx] = c
+    carts, board = extract_carts(board)
 
-    def extent( line):
-        m, M = None, None
-        for k,v in line.items():
-            if v == '.': continue
-            if m is None or k < m: m = k
-            if M is None or k > M: M = k
-        return m, M
+    print(carts)
+    print_board(carts, board)
+    for _ in range(10000):
+        first_crash_location = step(carts, board)
+        #print(carts)
+        #print_board(carts, board)
+        if first_crash_location is not None:
+            break
 
-    def pline( line,m,M):
-        result = ''
-        for idx in range(m,M+1):
-            result += line[idx]
-        print( result)
+    return first_crash_location[1],first_crash_location[0]
 
-    pad = 4
+def step2( carts, board):
+    positions = { cart.toPos():idx for idx,cart in enumerate(carts)}
 
-    def step(line,m,M):
-        newline = defaultdict(lambda: '.')
-        for idx in range(m-pad,M+pad-5+1+1):
-            for lhs, rhs in rules:
-                assert len(lhs) == 5
-                match = True
-                for i,c in enumerate(lhs):
-                    if line[idx+i] != c:
-                        match = False
-                if match:
-                    assert len(rhs) == 1
-                    newline[idx+2] = rhs
-        return newline
+    crashed_carts = set()
 
-    def count(line):
-        count = 0
-        for k,v in line.items():
-            if v == '#':
-                count += k
-        return count
+    for cart_idx,cart in enumerate(carts):
+        print( cart_idx, cart, positions, crashed_carts)
+        if cart_idx in crashed_carts: continue
+        
+        # Should always be able to go forward one
+        jrow,jcol = cart.irow+cart.drow,cart.icol+cart.dcol
+        c = board[jrow][jcol] 
+        assert c != ' '
+        d = cart.drow,cart.dcol
+        if c == '+':
+            """change dir based on state"""
+            if cart.state == 'L':
+                cart.drow,cart.dcol = c2dir[left_turn[dir2c[d]]]
+            elif cart.state == 'R':
+                cart.drow,cart.dcol = c2dir[right_turn[dir2c[d]]]
+            elif cart.state == 'S':
+                pass
+            else:
+                assert False, (cart.drow,cart.dcol,cart.state)
+            cart.incr_state()
+        elif c == '/':
+            cart.drow,cart.dcol = c2dir[forward_slash[dir2c[d]]]
+        elif c == '\\':
+            cart.drow,cart.dcol = c2dir[backward_slash[dir2c[d]]]
+        else:
+            pass
 
-    m, M = extent(line)
-    pline(line,m,M)
-    for _ in range(20):
-        line = step(line,m,M)
-        m,M = extent(line)
-        pline(line,m,M)
+        del positions[cart.toPos()]
 
-    return count(line)
+        cart.irow,cart.icol = jrow,jcol
+        if cart.toPos() in positions:
+            crashed_carts.add(cart_idx)
+            crashed_carts.add(positions[cart.toPos()])
+            
+        positions[cart.toPos()] = cart_idx
 
+    carts = [ cart for idx,cart in enumerate(carts) if idx not in crashed_carts]
 
-def extent( line):
-    m, M = None, None
-    for k,v in line.items():
-        if v == '.': continue
-        if m is None or k < m: m = k
-        if M is None or k > M: M = k
-    return m, M
-
-def pline( line,m,M):
-    result = ''
-    for idx in range(m,M+1):
-        result += line[idx]
-    print( result)
-
-def count(line):
-    count = 0
-    for k,v in line.items():
-        if v == '#':
-            count += k
-    return count
-
-def step(rules,line,m,M):
-    pad = 4
-
-    newline = defaultdict(lambda: '.')
-    for idx in range(m-pad,M+pad-5+1+1):
-        for lhs, rhs in rules:
-            assert len(lhs) == 5
-            match = True
-            for i,c in enumerate(lhs):
-                if line[idx+i] != c:
-                    match = False
-            if match:
-                assert len(rhs) == 1
-                newline[idx+2] = rhs
-    return newline
-
-
-def check(initial_state,rules,n):
-
-    line = defaultdict(lambda: '.')
-
-    for idx,c in enumerate(initial_state):
-        if c == '#':
-            line[idx] = c
-
-    m, M = extent(line)
-    for _ in range(n):
-        line = step(rules,line,m,M)
-        m, M = extent(line)
-
-    return count(line)
+    carts.sort(key=lambda o: o.toPos())
+    return carts
 
 def main2(fp):
-    initial_state, rules = parse(fp)
-
-    line = defaultdict(lambda: '.')
-
-    for idx,c in enumerate(initial_state):
-        if c == '#':
-            line[idx] = c
+    board = parse(fp)
 
 
-    def freeze(line,m):
-        s = set()
-        for k,v in line.items():
-            if v == '#':
-                s.add(k-m)
-        return frozenset(s)
+    carts, board = extract_carts(board)
 
-    def counton(line):
-        count = 0
-        for k,v in line.items():
-            if v == '#':
-                count += 1
-        return count
+    assert len(carts) % 2 == 1
 
-    states = {}
+    print(carts)
+    print_board(carts, board)
+    for _ in range(1000000):
+        carts = step2(carts, board)
+        #print(carts)
+        #print_board(carts, board)
+        if len(carts) == 1:
+            p = carts[0].toPos()
+            return p[1],p[0]
 
-    m, M = extent(line)
-    pline(line,m,M)
+    return None
 
-    states[freeze(line,m)] = (0,count(line))
-
-    linear_fit = None
-
-    for i in range(1,2001):
-        line = step(rules,line,m,M)
-        m,M = extent(line)
-        print(i,m,M,' ',end='')
-        pline(line,m,M)
-        s = freeze(line,m)
-        score = count(line)
-        if s in states:
-            linear_fit = [ states[s], (i,score)]
-            break
-        else:
-            states[s] = (i,score)
-
-    print(linear_fit)
-
-    """
-    y - y0 = (y1-y0)/(x1-x0) (x - x0)
-"""
-    (x0,y0),(x1,y1) = tuple(linear_fit)
-    x = 50*1000*1000*1000
-    #x = 200
-    m = (y1-y0)//(x1-x0)
-    print( f'm: {m} counton: {counton(line)}')
-    y = y0 + m*(x-x0)
-
-    if x < 1000:
-        assert check( initial_state, rules, x) == y
-
-
-
-    return y
-
+@pytest.mark.skip
 def test_A():
     with open("data0","rt") as fp:
-        assert 325 == main(fp)
+        assert (7,3) == main(fp)
 
 @pytest.mark.skip
 def test_B():
     with open("data","rt") as fp:
-        print(main2(fp))
+        print(main(fp))
+
+def test_AA():
+    with open("data1","rt") as fp:
+        assert (6,4) == main2(fp)
 
 #@pytest.mark.skip
 def test_BB():
