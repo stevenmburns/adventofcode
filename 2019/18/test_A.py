ade9c30a8630db388fd7600a40c4004abfdd6e27
Steven Burns <smburns47@gmail.com>
2021-02-26 04:39:55 -0800

    Still slow

--- 2019/18/test_A.py
+++ 2019/18/test_A.py
@@ -4,8 +4,9 @@ import re
 import itertools
 from collections import defaultdict, deque
 import logging
+import heapq
 
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 def parse(fp):
     seq = []
@@ -19,12 +20,51 @@ def parse(fp):
 
     return seq
 
+def determine_path_lengths( board, p):
+    nrows = len(board)
+    ncols = len(board[0])
+
+    reached = set()
+    frontier = set( [p])
+    level = 0
+
+    length_tbl = { p: 0}
+
+    while frontier:
+
+        new_frontier = set()
+        for state in frontier:
+
+            (irow,icol) = state
+
+            dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+
+            for drow,dcol in dirs:
+                jrow,jcol = irow+drow,icol+dcol
+                if 0 <= jrow < nrows and 0 <= jcol < ncols:
+                    c = board[jrow][jcol]
+                    if c == '#': continue
+                    if c != '.':
+                        if (jrow,jcol) not in length_tbl:
+                            length_tbl[ (jrow,jcol)] = level+1
+                    else:
+                        new_frontier.add( (jrow,jcol))
+
+        reached = reached.union(frontier)
+        frontier = new_frontier.difference(reached)
+        level += 1
+    
+    return length_tbl
+
+
 def main(fp):
     board = parse(fp)
 
     nrows = len(board)
     ncols = len(board[0])
  
+    adjacents = {}
+
     start = set()
     all_keys = set()
     for irow,line in enumerate(board):
@@ -35,51 +75,62 @@ def main(fp):
             if c.islower():
                 all_keys.add( c)
 
+            if c == '@' or c.isupper() or c.islower():
+                adjacents[ (irow,icol)] = determine_path_lengths(board, (irow,icol))
+
+    print()
+    for k,v in adjacents.items():
+        print( k, board[k[0]][k[1]])
+        for kk,vv in v.items():
+            print( '\t', kk, board[kk[0]][kk[1]], vv)
+
     assert len(start) == 1
 
     state = frozenset(), list(start)[0]
 
+    reached = {} # state => steps
 
-    reached = set()
-    frontier = set( [state])
-    level = 0
-
-    best_keys = 0
+    frontier = []
+    heapq.heappush(frontier, (0, state))
 
     while frontier:
 
-        logging.info( f"{level+1} {best_keys} {len(all_keys)}: {len(reached)} {len(frontier)} {''.join(str(x) for x in frontier)}")
+        level, state = heapq.heappop( frontier)
 
-        new_frontier = set()
-        for state in frontier:
+        print( f'headpop: {level} {state}') 
 
-            keys_acquired, (irow,icol) = state
+        if state not in reached:
+            reached[state] = level
 
-            dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+        keys_acquired, (irow,icol) = state
 
-            for drow,dcol in dirs:
-                jrow,jcol = irow+drow,icol+dcol
-                if 0 <= jrow < nrows and 0 <= jcol < ncols:
-                    c = board[jrow][jcol]
-                    if c == '#': continue
-                    if c.isupper() and c.lower() not in keys_acquired: continue
-                    if c.islower():
-                        new_keys_acquired = set(keys_acquired)
-                        new_keys_acquired.add( c)
+        for (jrow,jcol),cost in adjacents[(irow,icol)].items():
+            new_cost = level + cost
 
-                        if len(new_keys_acquired) > best_keys:
-                            best_keys = len(new_keys_acquired)
+            if (jrow,jcol) == (irow,icol): continue
+ 
+            c = board[jrow][jcol]
+            assert c != '#'
+            if c.isupper() and c.lower() not in keys_acquired: continue
+            if c.islower():
+                new_keys_acquired = set(keys_acquired)
+                new_keys_acquired.add( c)
 
-                        if new_keys_acquired == all_keys:
-                            return level+1
+                if new_keys_acquired == all_keys:
+                    return new_cost
 
-                        new_frontier.add( (frozenset( new_keys_acquired), (jrow,jcol)))
-                    else:
-                        new_frontier.add( ( keys_acquired, (jrow,jcol)))
+                new_state = (frozenset( new_keys_acquired), (jrow,jcol))
+            else:
+                new_state = (keys_acquired, (jrow,jcol))
 
-        reached = reached.union(frontier)
-        frontier = new_frontier.difference(reached)
-        level += 1
+
+            if new_state not in reached or reached[new_state] > new_cost:
+                reached[new_state] = new_cost
+                heapq.heappush( frontier, (new_cost,new_state))
+
+
+
+
 
     return None
 
@@ -124,52 +175,73 @@ def main2(fp):
 
     board = new_board
 
+
+    adjacents = {}
+    for irow,line in enumerate(board):
+        assert ncols == len(line)
+        for icol,c in enumerate(line):
+            if c == '@' or c.isupper() or c.islower():
+                adjacents[ (irow,icol)] = determine_path_lengths(board, (irow,icol))
+
+    print()
+    for k,v in adjacents.items():
+        print( k, board[k[0]][k[1]])
+        for kk,vv in v.items():
+            print( '\t', kk, board[kk[0]][kk[1]], vv)
+
+
     state = frozenset(), (p0,p1,p2,p3)
 
-    reached = set()
-    frontier = set( [state])
-    level = 0
 
-    best_keys = 0
+    reached = {} # state => steps
+
+    frontier = []
+    heapq.heappush(frontier, (0, state))
 
     while frontier:
 
-        logging.info( f"{level+1} {best_keys} {len(all_keys)}: {len(reached)} {len(frontier)}")
+        level, state = heapq.heappop( frontier)
 
-        new_frontier = set()
-        for state in frontier:
-            keys_acquired, ps = state
+        logging.info( f'headpop: {level} {len(state[0])}') 
+
+        if state not in reached:
+            reached[state] = level
+
+        keys_acquired, ps = state
+
+        for idx in range(len(ps)):
+            irow,icol = ps[idx]
+            
+            for (jrow,jcol),cost in adjacents[(irow,icol)].items():
+                new_ps = list(ps)
+                new_ps[idx] = (jrow,jcol)
+                new_ps = tuple(new_ps)
+
+                new_cost = level + cost
+
+                if (jrow,jcol) == (irow,icol): continue
+
+                c = board[jrow][jcol]
+                assert c != '#'
+
+                if c.isupper() and c.lower() not in keys_acquired: continue
+                if c.islower():
+                    new_keys_acquired = set(keys_acquired)
+                    new_keys_acquired.add( c)
+
+                    if new_keys_acquired == all_keys:
+                        return new_cost
+
+                    new_state = (frozenset( new_keys_acquired), new_ps)
+                else:
+                    new_state = (keys_acquired, new_ps)
 
 
-            for idx in range(len(ps)):
-                irow,icol = ps[idx]
-                dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+                if new_state not in reached or reached[new_state] > new_cost:
+                    reached[new_state] = new_cost
+                    heapq.heappush( frontier, (new_cost,new_state))
 
-                ps_lst = list(ps)
-                for drow,dcol in dirs:
-                    jrow,jcol = irow+drow,icol+dcol
-                    ps_lst[idx] = (jrow,jcol)
-                    if 0 <= jrow < nrows and 0 <= jcol < ncols:
-                        c = board[jrow][jcol]
-                        if c == '#': continue
-                        if c.isupper() and c.lower() not in keys_acquired: continue
-                        if c.islower():
-                            new_keys_acquired = set(keys_acquired)
-                            new_keys_acquired.add( c)
 
-                            if len(new_keys_acquired) > best_keys:
-                                best_keys = len(new_keys_acquired)
-
-                            if new_keys_acquired == all_keys:
-                                return level+1
-
-                            new_frontier.add( (frozenset( new_keys_acquired), tuple(ps_lst)))
-                        else:
-                            new_frontier.add( ( keys_acquired, tuple(ps_lst)))
-
-        reached = reached.union(frontier)
-        frontier = new_frontier.difference(reached)
-        level += 1
 
     return None
 
@@ -224,7 +296,7 @@ def test_AA3():
         assert 72 == main2(fp)
 
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_BB():
     with open("data","rt") as fp:
         print( main2(fp))
